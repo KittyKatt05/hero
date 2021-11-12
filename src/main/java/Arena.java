c969fe0128bef5768d24abfad54518ea403e4bae
Catarina <1201338@isep.ipp.pt>
2021-11-12 16:02:19 +0000

    Painted the screen and the Hero

--- src/main/java/Arena.java
+++ src/main/java/Arena.java
@@ -1,20 +1,30 @@
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.screen.Screen;
 
 public class Arena {
 
+
     private int width;
     private int height;
     private Hero hero;
+
 
     public Arena(int width, int height) {
         this.width = width;
         this.height = height;
         hero = new Hero(10,10);
+
+
     }
 
-    public void draw(Screen screen) {
-        hero.draw(screen);
+    public void draw(TextGraphics graphics) {
+        graphics.setBackgroundColor(TextColor.Factory.fromString("#336699"));
+        graphics.fillRectangle(new TerminalPosition(0, 0), new TerminalSize(width, height), ' ');
+        hero.draw(graphics);
     }
 
     public void processKey(KeyStroke key) {
